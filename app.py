5867b9dd92337149722cb615d1c6f0f1a5b80c6d
Dalal15 <dcchahuan@uc.cl>
2019-03-21 11:15:32 -0300

    info desordenada

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from starwars import (get_films,
+                      get_planet,
+
+                      get_ship,)
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    context = {
+        "films": get_films()['results'],
+        "planet_name": get_planet(1)['name'],
+        "ship_length": get_ship(3)['length'],
+        "ship_name": get_ship(3)['name']
+    }
+    return render_template("index.html", **context)
 
 if __name__ == '__main__':
     app.run()
